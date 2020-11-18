903ccbc3ad09f903316e49c50571d94d228020a2
ArmandoGarciaHernandez <agarcia115@ucol.mx>
2020-11-18 01:45:23 -0600

    delete modules folder

--- Modular/10AM+2500HRS.js
+++
@@ -1,9 +0,0 @@
-let horaI = 10;
-let HoraF = 2500;
-let suma = horaI + HoraF;
-let modulo = 24;
-
-console.log('Si son la 10 AM, ¿qué horas serán en 2500hrs?');
-console.log('Serían la(s) ' + (suma % modulo) + (' hr(s) del día'));
-
-//Formato 24 hrs
